4f7a2368001c625a87bce5c57fe98884f19d57e3
Raiyan <tajkierhaque@gmail.com>
2023-11-30 22:51:54 +0600

    almost done

---
+++ README.md
@@ -0,0 +1,1 @@
+https://docs.google.com/document/d/1AAn7VCrXDMr5xBmS8Hfu-lFXxl79sHKBPNvCo109kRA/edit
---
+++ restaurant/about_us/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def aboutUs(request):
+    return render(request, 'about_us/about.html')
